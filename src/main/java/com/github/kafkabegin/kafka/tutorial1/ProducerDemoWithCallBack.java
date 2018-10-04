2298950cff1103e8a8d6eb9b1cfe368efe084a0e
Thadeu Costa <Thadeu.Costa@vistajet.com>
2018-10-04 11:12:31 -0300

    Producer with callback

---
+++ src/main/java/com/github/kafkabegin/kafka/tutorial1/ProducerDemoWithCallBack.java
@@ -0,0 +1,40 @@
+package com.github.kafkabegin.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallBack {
+    public static void main(String[] args) {
+
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallBack.class);
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        final KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        for(int i = 0 ; i < 10; i ++) {
+            ProducerRecord<String, String> record = new
+                    ProducerRecord<String, String>("first_topic", "hello kafka, fom java " + Integer.toString(i));
+
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    if (e == null) {
+                        logger.info("Received metadata. \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    }
+                }
+            });
+        }
+        producer.flush();
+        producer.close();
+    }
+}
