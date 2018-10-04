67292e4a76150ef3fb9911054b67531d8278705b
Thadeu Costa <Thadeu.Costa@vistajet.com>
2018-10-04 10:06:45 -0300

    First Commit

---
+++ src/main/java/com/github/kafkabegin/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,15 @@
+package com.github.kafkabegin.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    }
+}
