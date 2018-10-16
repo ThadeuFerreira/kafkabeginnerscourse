71a1d6c9b9163b533ef9655288f30e70b105cf9c
Thadeu Costa <Thadeu.Costa@vistajet.com>
2018-10-16 09:50:53 -0300

    refactor folders

--- kafka-producer-twitter/src/main/java/com/github/kafkabegin/kafka/tutorial2/TwitterProducer.java
+++ kafka-producer-twitter/src/main/java/com/github/kafkabegin/kafka/tutorial2/TwitterProducer.java
@@ -1,6 +1,5 @@
 package com.github.kafkabegin.kafka.tutorial2;
 
-import com.github.kafkabegin.kafka.tutorial1.ProducerDemoWithCallBack;
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
@@ -107,6 +106,16 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        //create "Safe Producer"
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString((Integer.MAX_VALUE)));
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        //create High Throughput
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); //add latency
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024)); //32KB
         return new KafkaProducer<String, String>(properties);
 
 
