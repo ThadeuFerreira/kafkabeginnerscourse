8cb2af7ad82c5c032386afee78909aefe68601b2
Thadeu Costa <Thadeu.Costa@vistajet.com>
2018-10-17 09:30:31 -0300

    add elastic search consumer

--- kafka-consumer-elastic/src/main/java/com/github/simplesteph/kafka/tutorial3/ElasticSearchConsumer.java
+++ kafka-consumer-elastic/src/main/java/com/github/simplesteph/kafka/tutorial3/ElasticSearchConsumer.java
@@ -6,16 +6,24 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public class ElasticSearchConsumer {
     public static RestHighLevelClient createClient(){
 
-        String hostname = "";
-        String username = "";
-        String password = "";
+        String hostname = "thadeu-kafka-6787423158.us-east-1.bonsaisearch.net";
+        String username = "fb0x74taq6";
+        String password = "80nhbd5b7u";
 
         final CredentialsProvider  credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username,password));
@@ -29,12 +37,24 @@ public class ElasticSearchConsumer {
                     }
                 });
 
-        RestHighLevelClient client = new RestHighLevelClient(builder);
-        return client;
+        return  new RestHighLevelClient(builder);
+
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class);
+        RestHighLevelClient client = createClient();
 
+        String jsonString = "{\"foo\": \"bar\"}";
+        IndexRequest indexRequest = new IndexRequest(
+                "twitter", "tweets"
+        ).source(jsonString, XContentType.JSON);
+
+        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+        String id = indexResponse.getId();
+        logger. info(id);
+
+        client.close();
     }
 }
